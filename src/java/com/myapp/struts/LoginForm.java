f48c88659429a7af3e4c6a1e86d094524ae1fce8
byde <neoodiseo@gmail.com>
2010-11-16 20:45:15 -0600

    ActionForm y success-jsp

---
+++ src/java/com/myapp/struts/LoginForm.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.myapp.struts;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+/**
+ *
+ * @author lugubrenator
+ */
+public class LoginForm extends org.apache.struts.action.ActionForm {
+
+    private String name;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    /**
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param string
+     */
+    public void setName(String string) {
+        name = string;
+    }
+
+    /**
+     *
+     */
+    public LoginForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        if (getName() == null || getName().length() < 1) {
+            errors.add("name", new ActionMessage("error.name.required"));
+            // TODO: add 'error.name.required' key to your resources
+        }
+        return errors;
+    }
+}
---
+++ web/WEB-INF/success.jsp
@@ -0,0 +1,27 @@
+<%--
+    Document   : success
+    Created on : 16/11/2010, 08:17:10 PM
+    Author     : Karen
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Login Success</title>
+  </head>
+  <body>
+      <h1>Felicidades!!!</h1>
+      <p>Has iniciado Sesión </p>
+      <p>Tu nombre es: </p>
+      <p>Tu correo electrónico es: </p>
+
+      <p>Tu nombre es:  <bean:write name="LoginForm" property="name" />.</p>
+      <p>Tu correo electrónico es:  <bean:write name="LoginForm" property="email" />.</p>
+  </body>
+</html>
